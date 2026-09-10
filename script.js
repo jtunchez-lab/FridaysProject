b61d2722692dd23cd5ee854592f597dcdbd14149
jtunchez-lab <jesus.tunchez@rloacs.org>
2026-09-10 08:11:57 -0400

    Implement number guessing game logic

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const SecretNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 0
+let Guess = 0
 
+do {
+    
+    guess = parseInt(prompt("Enter your guess:"));
+    attempts++;
+
+    
+    if (guess < secretNumber) {
+        alert("Too low Try again.");
+    } else if (guess > secretNumber) {
+        alert("Too high Try again.");
+    } else {
+        alert(` Correct! You got it in ${attempts} attempts.`);
+    }
+
+} while (guess !== secretNumber);
